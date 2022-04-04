6de3937a7b92d37ca3f6c2ddc9fd4c73cc99f4a1
skalman10 <isak.thyrel@elev.ga.ntig.se>
2022-04-04 10:53:32 +0200

    🚀 POST route

--- routes/meeps.js
+++ routes/meeps.js
@@ -4,25 +4,53 @@ const router = express.Router();
 const pool = require('../database');
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
     await pool.promise()
         .query(`SELECT * FROM meeps`)
-        .then(([rows,fields]) => {
+        .then(([rows, fields]) => {
             console.log(rows);
             res.render('meeps.njk', {
                 meeps: rows,
                 title: 'Meeps',
                 layout: 'layout.njk'
-              });
+            });
         })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    tasks: {
-                        error: `Error getting meeps`
-                    }
-                })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: `Error getting meeps`
+                }
             })
         });
+});
+
+router.post('/', async (req, res, next) => {
+    const meep = req.body.body;
+
+    await pool.promise()
+        .query('INSERT INTO meeps (body) VALUES (?)', [meep])
+        .then((response) => {
+            console.log(response[0].affectedRows);
+            if (response[0].affectedRows === 1) {
+                res.redirect('/meeps');
+            } else {
+                res.status(400).json({
+                    meeps: {
+                        error: 'Invalid meep'
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                meeps: {
+                    error: 'Error posting meep'
+                }
+            })
+        });
+}
+);
 
 module.exports = router;
